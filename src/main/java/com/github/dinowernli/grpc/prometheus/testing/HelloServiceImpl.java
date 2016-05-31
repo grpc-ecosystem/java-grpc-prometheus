81fed7db094cbf362ef6f1247485a7dfded309fd
Dino Wernli <dinowernli@gmail.com>
2016-05-31 08:14:00 +0100

    Implement the remaining request types for testing.

--- src/main/java/com/github/dinowernli/grpc/prometheus/testing/HelloServiceImpl.java
+++ src/main/java/com/github/dinowernli/grpc/prometheus/testing/HelloServiceImpl.java
@@ -8,6 +8,12 @@ import polyglot.HelloProto.HelloResponse;
 import polyglot.HelloServiceGrpc.HelloService;
 
 public class HelloServiceImpl implements HelloService {
+  /**
+   * Indicates how many request messages we wait for before responding to the client in the case of
+   * the client streamed method.
+   */
+  private static final int CLIENT_STREAM_NUM_REQUESTS = 5;
+
   @Override
   public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
     responseObserver.onNext(HelloResponse.newBuilder()
@@ -17,10 +23,71 @@ public class HelloServiceImpl implements HelloService {
   }
 
   @Override
-  public void sayHelloStream(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-    responseObserver.onNext(HelloResponse.newBuilder()
-        .setMessage("Hello, " + request.getRecipient())
-        .build());
+  public void sayHelloServerStream(
+      HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+    responseObserver.onNext(responseForRequest(request));
     responseObserver.onCompleted();
+  }
+
+  @Override
+  public StreamObserver<HelloRequest> sayHelloBidiStream(
+      StreamObserver<HelloResponse> responseStream) {
+    return new StreamObserver<HelloRequest>() {
+      @Override
+      public void onCompleted() {
+        // When the client stops sending requests, we stop sending responses.
+        responseStream.onCompleted();
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        // Client error, propagate.
+        responseStream.onError(t);
+      }
+
+      @Override
+      public void onNext(HelloRequest request) {
+        // Response to each request with exactly one response.
+        responseStream.onNext(responseForRequest(request));
+      }
+    };
+  }
+
+  @Override
+  public StreamObserver<HelloRequest> sayHelloClientStream(
+      StreamObserver<HelloResponse> responseStream) {
+    return new StreamObserver<HelloRequest>() {
+      int numReceivedRequests = 0;
+
+      @Override
+      public void onCompleted() {
+        // When the client stops sending requests, we stop sending responses.
+        responseStream.onCompleted();
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        // Client error, propagate.
+        responseStream.onError(t);
+      }
+
+      @Override
+      public void onNext(HelloRequest request) {
+        ++numReceivedRequests;
+        if (numReceivedRequests >= CLIENT_STREAM_NUM_REQUESTS) {
+          responseStream.onNext(responseForRequest(request));
+          responseStream.onCompleted();
+        }
+      }
+    };
+  }
+
+  private HelloResponse responseForRequest(HelloRequest request) {
+    if (request.getThrowException()) {
+      throw new RuntimeException("I was told to throw!");
+    }
+    return HelloResponse.newBuilder()
+        .setMessage("Hello, " + request.getRecipient())
+        .build();
   }
 }
